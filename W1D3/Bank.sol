1f68f0b691c28da1b8927aa33ba7053f539ffcd0
CL <105766275+colorlessballoon@users.noreply.github.com>
2025-04-30 16:28:04 +0800

    Add files via upload

---
+++ W1D3/Bank.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract Bank{
+    mapping(address => uint) public balances;
+    address[3] public top3;
+    address public immutable owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function withDraw(uint256 amount) public payable{
+        require(msg.sender == owner, "Only the owner can withdraw");
+        require(amount <= address(this).balance, "Insufficient balance in the contract");
+        payable(owner).transfer(amount);
+        
+    }
+    
+    function deposit() public payable{
+        require(msg.value >= 0, "Transfer amount must be greater than 0");
+        balances[msg.sender] += msg.value;
+        updateTop3();
+    }
+
+    function updateTop3() internal {
+        for(uint i = 0; i < top3.length; i++){
+            if(msg.sender == top3[i]) break;
+            if (balances[msg.sender] > balances[top3[i]]) {
+                for (uint j = top3.length - 1; j > i; j--) {
+                    top3[j] = top3[j - 1];
+                }
+                top3[i] = msg.sender;
+                break;
+            }
+        }
+    }
+
+    receive() external payable {
+        deposit();
+    }
+}
---
+++ W1D3/readme.md
@@ -0,0 +1,5 @@
+合约部署：[Sepolia Transaction Hash: 0xc45c53a5d2... | Etherscan](https://sepolia.etherscan.io/tx/0xc45c53a5d20248b0141b11c61983c5d54e6e2b314af79ebaea75b8a86ca597c9)
+
+向合约转入ETH：[Sepolia Transaction Hash: 0x6e0b87c5b6... | Etherscan](https://sepolia.etherscan.io/tx/0x6e0b87c5b630d1479231616e79dcedc9d1c4a40b305ca2532e066f2f9348cdb4)
+
+管理员提出ETH：[Sepolia Transaction Hash: 0xd1cbe5e636... | Etherscan](https://sepolia.etherscan.io/tx/0xd1cbe5e636c2bd194828c3d4830f0701fd22f69b55099e303a5a2927d45f54b4)
